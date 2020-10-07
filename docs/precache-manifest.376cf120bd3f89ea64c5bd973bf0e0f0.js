a3274028a7689b2d5052c9f4e4a23536a70e3caf
mitchellkealy <60372669+mitchellkealy@users.noreply.github.com>
2020-10-07 13:04:42 -0500

    GitHub pages publish

---
+++ docs/precache-manifest.376cf120bd3f89ea64c5bd973bf0e0f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b13252362511a952cff40cc4092317",
+    "url": "/crypto-app/index.html"
+  },
+  {
+    "revision": "7ced2cc86f853fa1092a",
+    "url": "/crypto-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "976393e9f8bc07abf187",
+    "url": "/crypto-app/static/js/2.29d56ffa.chunk.js"
+  },
+  {
+    "revision": "3d4d9872e604682782346535cc277e64",
+    "url": "/crypto-app/static/js/2.29d56ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ced2cc86f853fa1092a",
+    "url": "/crypto-app/static/js/main.06e2e4a9.chunk.js"
+  },
+  {
+    "revision": "79bc0aaddad1c598addb",
+    "url": "/crypto-app/static/js/runtime-main.36667200.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crypto-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
